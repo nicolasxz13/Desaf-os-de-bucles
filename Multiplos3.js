16efa80e3d294b934658218ad989dab1ce3e1c30
nicolasxz13 <nicolasxz13@gmail.com>
2023-09-10 20:42:21 -0300

    first commit

---
+++ Multiplos3.js
@@ -0,0 +1,6 @@
+//Disminuir múltiplos de 3
+for (let i = 100; i >= 0; i--) {
+    if (i % 3 === 0 && i !== 0) {
+        console.log(i);
+    }
+}
---
+++ Secuencia.js
@@ -0,0 +1,4 @@
+var valores = [4,2.5,1,-0.5,-2,-3.5];
+for (let i = 0; i < valores.length; i++) {
+  console.log(valores[i]);
+}
